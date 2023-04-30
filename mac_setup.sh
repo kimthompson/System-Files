a11d427096e86a8a28e80746b6bdd6e08f3b2997
Kim Thompson <kimthompson@hey.com>
2023-04-30 07:07:10 -0500

    Create mac_setup.sh

---
+++ mac_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# update everything
+brew update
+brew upgrade
+brew cleanup -s
+brew cask cleanup
+
+# install my most beloved tools
+NONINTERACTIVE=1 brew install gh fd ripgrep zsh tmux lazygit neovim
+
+# configure zsh, tmux, and neovim
+curl https://raw.githubusercontent.com/kimthompson/system_files/master/.tmux.conf --output ~/.tmux.conf
+curl https://raw.githubusercontent.com/kimthompson/system_files/master/.zshrc --output ~/.zshrc
+git clone https://github.com/LazyVim/starter ~/.config/nvim/
+rm -rf ~/.config/nvim/.git
+curl https://raw.githubusercontent.com/kimthompson/system_files/master/custom.lua --output ~/.config/nvim/lua/plugins/custom.lua
+
+# and now, finally, boot into zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
