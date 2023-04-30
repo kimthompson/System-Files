4c7a9a74fa1bd7443fe72a00ebf0b9d725120ffc
Kim Thompson <kimthompson@hey.com>
2023-04-30 08:52:09 -0500

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -18,6 +18,9 @@ curl https://raw.githubusercontent.com/kimthompson/system_files/master/.zshrc --
 git clone https://github.com/LazyVim/starter ~/.config/nvim/
 rm -rf ~/.config/nvim/.git
 curl https://raw.githubusercontent.com/kimthompson/system_files/master/custom.lua --output ~/.config/nvim/lua/plugins/custom.lua
+rm -rf ~/.config/nvim/lua/plugins/example.lua
+rm -rf ~/.config/nvim/lua/plugins/keymaps.lua
+curl https://raw.githubusercontent.com/kimthompson/system_files/master/keymaps.lua --output ~/.config/nvim/lua/config/keymaps.lua
 
 # and now, finally, boot into zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
